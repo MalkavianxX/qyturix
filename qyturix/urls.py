15011dbd1f1ff3ff883064269020e8ad4e6b292d
MalkavianxX <mot46000@gmail.com>
2024-02-16 01:57:17 -0600

    dd

--- qyturix/urls.py
+++ qyturix/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('camaras/',include('camaras.urls')),
     path('evento/',include('evento.urls')),
     path('accounts/', include('allauth.urls')),
+    path('consulta/',include('consulta.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
